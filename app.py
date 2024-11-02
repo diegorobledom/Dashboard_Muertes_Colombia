e9a3037ff583f735ea2f97d49a57b34f8cc55e71
Diego Robledo <diegorobledomejia@gmail.com>
2024-11-01 21:40:58 -0500

    Despliegue inicial en Render

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ app.layout = html.Div([
 
 # Ejecutar el servidor
 if __name__ == '__main__':
-    app.run_server(debug=True, port=int(os.environ.get("PORT", 8080)), host='0.0.0.0')
+    app.run_server(debug=True)
